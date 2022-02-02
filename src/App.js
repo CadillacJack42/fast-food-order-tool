7a3be62a3b991432878f4580006bcb0f479a5bb1
James Keicher <jamesdkeicher@gmail.com>
2022-02-01 16:02:23 -0800

    feat: render customer name on

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import { useState } from 'react';
 import './App.css';
 import DropDownGen from './DropDownGen';
+import OrderName from './OrderName';
 
 function App() {
   const [foodId, setFoodId] = useState(1);
   // const [sideId, setSideId] = useState(1);
   // const [drinkId, setDrinkId] = useState(1);
   // const [instructions, setInstructions] = useState([]);
-  // const [orderName, setOrderName] = useState('Valued Customer');
+  const [orderName, setOrderName] = useState('Valued Customer');
   return (
     <div className="App">
+      <OrderName setOrderName={setOrderName}/>
+      <p>Order for: {orderName}</p>
       <DropDownGen 
         setter={setFoodId}
         value={'food'}
